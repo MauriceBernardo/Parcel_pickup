7f235d3f72dbb96a0b03878bb265ea7061b62e0e
Maurice <Maurice_tikus@hotmail.com>
2019-05-25 14:51:47 +1000

    create new branch

--- src/mycontroller/MyAutoController.java
+++ src/mycontroller/MyAutoController.java
@@ -1,18 +1,24 @@
 package mycontroller;
 
+import com.badlogic.gdx.Input;
 import controller.CarController;
+import swen30006.driving.Simulation;
+import tiles.TrapTile;
 import world.Car;
 import java.util.HashMap;
+import java.util.Set;
 
 import tiles.MapTile;
 import utilities.Coordinate;
+import world.World;
 import world.WorldSpatial;
 
 public class MyAutoController extends CarController{		
 		// How many minimum units the wall is away from the player.
 		private int wallSensitivity = 1;
-		
-		private boolean isFollowingWall = false; // This is set to true when the car starts sticking to a wall.
+
+		// This is set to true when the car starts sticking to a wall.
+		private boolean isFollowingWall = false;
 		
 		// Car Speed to move at
 		private final int CAR_MAX_SPEED = 1;
@@ -27,127 +33,51 @@ public class MyAutoController extends CarController{
 		public void update() {
 			// Gets what the car can see
 			HashMap<Coordinate, MapTile> currentView = getView();
-			
-			// checkStateChange();
-			if(getSpeed() < CAR_MAX_SPEED){       // Need speed to turn and progress toward the exit
-				applyForwardAcceleration();   // Tough luck if there's a wall in the way
-			}
-			if (isFollowingWall) {
-				// If wall no longer on left, turn left
-				if(!checkFollowingWall(getOrientation(), currentView)) {
-					turnLeft();
-				} else {
-					// If wall on left and wall straight ahead, turn right
-					if(checkWallAhead(getOrientation(), currentView)) {
-						turnRight();
-					}
-				}
-			} else {
-				// Start wall-following (with wall on left) as soon as we see a wall straight ahead
-				if(checkWallAhead(getOrientation(),currentView)) {
-					turnRight();
-					isFollowingWall = true;
-				}
-			}
-		}
+			System.out.println(getPosition());
+			Coordinate carPosition = new Coordinate(getPosition());
 
-		/**
-		 * Check if you have a wall in front of you!
-		 * @param orientation the orientation we are in based on WorldSpatial
-		 * @param currentView what the car can currently see
-		 * @return
-		 */
-		private boolean checkWallAhead(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> currentView){
-			switch(orientation){
-			case EAST:
-				return checkEast(currentView);
-			case NORTH:
-				return checkNorth(currentView);
-			case SOUTH:
-				return checkSouth(currentView);
-			case WEST:
-				return checkWest(currentView);
-			default:
-				return false;
+			for(int y = carPosition.y + 4; y >= carPosition.y-4; y--){
+				for(int x = carPosition.x - 4; x <= carPosition.x+4; x++){
+					if(x == carPosition.x && y == carPosition.y){
+						System.out.printf("%6s","here");
+						System.out.print("|");
+						continue;
+					}
+
+					if(currentView.get(new Coordinate(x,y)).getType() == MapTile.Type.TRAP){
+						TrapTile test = (TrapTile) currentView.get(new Coordinate(x,y));
+						System.out.printf("%6s",test.getTrap());
+					} else {
+						System.out.printf("%6s", currentView.get(new Coordinate(x, y)).getType());
+					}
+					System.out.print("|");
+				}
+				System.out.println();
 			}
-		}
-		
-		/**
-		 * Check if the wall is on your left hand side given your orientation
-		 * @param orientation
-		 * @param currentView
-		 * @return
-		 */
-		private boolean checkFollowingWall(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> currentView) {
-			
-			switch(orientation){
-			case EAST:
-				return checkNorth(currentView);
-			case NORTH:
-				return checkWest(currentView);
-			case SOUTH:
-				return checkEast(currentView);
-			case WEST:
-				return checkSouth(currentView);
-			default:
-				return false;
-			}	
-		}
-		
-		/**
-		 * Method below just iterates through the list and check in the correct coordinates.
-		 * i.e. Given your current position is 10,10
-		 * checkEast will check up to wallSensitivity amount of tiles to the right.
-		 * checkWest will check up to wallSensitivity amount of tiles to the left.
-		 * checkNorth will check up to wallSensitivity amount of tiles to the top.
-		 * checkSouth will check up to wallSensitivity amount of tiles below.
-		 */
-		public boolean checkEast(HashMap<Coordinate, MapTile> currentView){
-			// Check tiles to my right
-			Coordinate currentPosition = new Coordinate(getPosition());
-			for(int i = 0; i <= wallSensitivity; i++){
-				MapTile tile = currentView.get(new Coordinate(currentPosition.x+i, currentPosition.y));
-				if(tile.isType(MapTile.Type.WALL)){
-					return true;
+
+			Set<Integer> parcels = Simulation.getParcels();
+			Simulation.resetParcels();
+			for (int k : parcels){
+				switch (k){
+					case Input.Keys.B:
+						applyBrake();
+						break;
+					case Input.Keys.UP:
+						applyForwardAcceleration();
+						break;
+					case Input.Keys.DOWN:
+						applyReverseAcceleration();
+						break;
+					case Input.Keys.LEFT:
+						turnLeft();
+						break;
+					case Input.Keys.RIGHT:
+						turnRight();
+						break;
+					default:
 				}
 			}
-			return false;
+
+
 		}
-		
-		public boolean checkWest(HashMap<Coordinate,MapTile> currentView){
-			// Check tiles to my left
-			Coordinate currentPosition = new Coordinate(getPosition());
-			for(int i = 0; i <= wallSensitivity; i++){
-				MapTile tile = currentView.get(new Coordinate(currentPosition.x-i, currentPosition.y));
-				if(tile.isType(MapTile.Type.WALL)){
-					return true;
-				}
-			}
-			return false;
-		}
-		
-		public boolean checkNorth(HashMap<Coordinate,MapTile> currentView){
-			// Check tiles to towards the top
-			Coordinate currentPosition = new Coordinate(getPosition());
-			for(int i = 0; i <= wallSensitivity; i++){
-				MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y+i));
-				if(tile.isType(MapTile.Type.WALL)){
-					return true;
-				}
-			}
-			return false;
-		}
-		
-		public boolean checkSouth(HashMap<Coordinate,MapTile> currentView){
-			// Check tiles towards the bottom
-			Coordinate currentPosition = new Coordinate(getPosition());
-			for(int i = 0; i <= wallSensitivity; i++){
-				MapTile tile = currentView.get(new Coordinate(currentPosition.x, currentPosition.y-i));
-				if(tile.isType(MapTile.Type.WALL)){
-					return true;
-				}
-			}
-			return false;
-		}
-		
 	}
