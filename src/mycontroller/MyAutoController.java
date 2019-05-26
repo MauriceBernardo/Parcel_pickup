e57ead7291c7ca10bed6b6c6ddad858ced7870cf
Maurice <Maurice_tikus@hotmail.com>
2019-05-26 22:06:49 +1000

    Added LocalMap to MyAutoController

--- src/mycontroller/MyAutoController.java
+++ src/mycontroller/MyAutoController.java
@@ -5,7 +5,9 @@ import controller.CarController;
 import swen30006.driving.Simulation;
 import tiles.TrapTile;
 import world.Car;
+
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import tiles.MapTile;
@@ -13,71 +15,69 @@ import utilities.Coordinate;
 import world.World;
 import world.WorldSpatial;
 
-public class MyAutoController extends CarController{		
-		// How many minimum units the wall is away from the player.
-		private int wallSensitivity = 1;
+public class MyAutoController extends CarController {
+    // Car explored map
+    private HashMap<Coordinate, MapTile> localMap = new HashMap<>();
 
-		// This is set to true when the car starts sticking to a wall.
-		private boolean isFollowingWall = false;
-		
-		// Car Speed to move at
-		private final int CAR_MAX_SPEED = 1;
-		
-		public MyAutoController(Car car) {
-			super(car);
-		}
-		
-		// Coordinate initialGuess;
-		// boolean notSouth = true;
-		@Override
-		public void update() {
-			// Gets what the car can see
-			HashMap<Coordinate, MapTile> currentView = getView();
-			System.out.println(getPosition());
-			Coordinate carPosition = new Coordinate(getPosition());
+    public MyAutoController(Car car) {
+        super(car);
+        if (Simulation.toConserve() == Simulation.StrategyMode.FUEL) {
+        }
 
-			for(int y = carPosition.y + 4; y >= carPosition.y-4; y--){
-				for(int x = carPosition.x - 4; x <= carPosition.x+4; x++){
-					if(x == carPosition.x && y == carPosition.y){
-						System.out.printf("%6s","here");
-						System.out.print("|");
-						continue;
-					}
+        // Initialisation of localMap
+        int maxWidth = mapWidth();
+        int maxHeight = mapHeight();
+        for (int y = 0; y <= maxHeight; y++) {
+            for (int x = 0; x <= maxWidth; x++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                MapTile unexploredTile = new MapTile(MapTile.Type.EMPTY);
+                localMap.put(coordinate, unexploredTile);
+            }
+        }
 
-					if(currentView.get(new Coordinate(x,y)).getType() == MapTile.Type.TRAP){
-						TrapTile test = (TrapTile) currentView.get(new Coordinate(x,y));
-						System.out.printf("%6s",test.getTrap());
-					} else {
-						System.out.printf("%6s", currentView.get(new Coordinate(x, y)).getType());
-					}
-					System.out.print("|");
-				}
-				System.out.println();
-			}
+    }
 
-			Set<Integer> parcels = Simulation.getParcels();
-			Simulation.resetParcels();
-			for (int k : parcels){
-				switch (k){
-					case Input.Keys.B:
-						applyBrake();
-						break;
-					case Input.Keys.UP:
-						applyForwardAcceleration();
-						break;
-					case Input.Keys.DOWN:
-						applyReverseAcceleration();
-						break;
-					case Input.Keys.LEFT:
-						turnLeft();
-						break;
-					case Input.Keys.RIGHT:
-						turnRight();
-						break;
-					default:
-				}
-			}
+    @Override
+    public void update() {
+        // Gets what the car can see
+        HashMap<Coordinate, MapTile> currentView = getView();
+        // Update the local map based on what the car can see
+        updateLocalMap(currentView);
 
+        // For logging purposes
+        Coordinate carPosition = new Coordinate(getPosition());
+        System.out.println(carPosition);
+        for (int y = carPosition.y + 4; y >= carPosition.y - 4; y--) {
+            for (int x = carPosition.x - 4; x <= carPosition.x + 4; x++) {
+                if (x == carPosition.x && y == carPosition.y) {
+                    System.out.printf("%6s", "here");
+                    System.out.print("|");
+                    continue;
+                }
 
-		}
-	}
+                if (currentView.get(new Coordinate(x, y)).getType() == MapTile.Type.TRAP) {
+                    TrapTile test = (TrapTile) currentView.get(new Coordinate(x, y));
+                    System.out.printf("%6s", test.getTrap());
+                } else {
+                    System.out.printf("%6s", currentView.get(new Coordinate(x, y)).getType());
+                }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+
+    private void updateLocalMap(HashMap<Coordinate, MapTile> currentView) {
+        Coordinate carPosition = new Coordinate(getPosition());
+        for (int y = carPosition.y + 4; y >= carPosition.y - 4; y--) {
+            for (int x = carPosition.x - 4; x <= carPosition.x + 4; x++) {
+                Coordinate viewCoordinate = new Coordinate(x, y);
+                this.localMap.put(viewCoordinate, currentView.get(viewCoordinate));
+            }
+        }
+    }
+
+    public HashMap<Coordinate, MapTile> getLocalMap() {
+        return localMap;
+    }
+}
